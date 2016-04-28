e5b97fd561b2653d296431f55f49d6de583b0c7a
dagfinnr <dagfinn@startsiden.no>
2016-04-28 09:49:41 +0200

    Rudimentary Mountebank.pm
    This has two purposes:
    
    1. Making it possible to include the module simply by 'use Test::Mountebank'.
    2. Documentation for the Test::Mountebank as a whole.

---
+++ lib/Test/Mountebank.pm
@@ -0,0 +1,41 @@
+package Test::Mountebank;
+
+=head1 NAME
+
+Test::Mountebank - Perl client library for mountebank
+
+=head1 SYNOPSIS
+
+  use Test::Mountebank;
+
+  # synopsis...
+
+=head1 DESCRIPTION
+
+# longer description...
+
+
+=head1 INTERFACE
+
+
+=head1 DEPENDENCIES
+
+
+=head1 SEE ALSO
+
+=cut
+
+use Test::Mountebank::Client;
+
+
+=head1 AUTHOR
+
+Dagfinn Reiersøl dagfinn@reiersol.com
+
+=head1 COPYRIGHT
+
+Copyright (C) 2016, Dagfinn Reiersøl.
+
+=cut
+
+1;
